03bace576ca53a75a09ccdd918c613c97642184a
Dmitriy Shekhovtsov <valorkin@gmail.com>
2021-09-13 15:49:56 +0300

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sap-ui5-fun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
